acd4699fbafeeb9ba06c21c3fab7d63761258195
bafubafu-bafuu <125217279+bafubafu-bafuu@users.noreply.github.com>
2023-10-13 11:14:04 +0900

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -35,7 +35,8 @@ let fruitSlowDown = false;
 let clickChangeFruit = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+//let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+let adLink = 'https://missav.com/ja/icd-119';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
 document.getElementsByTagName("title")[0].innerText = 'スイカゲーム';
